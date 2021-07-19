d360863c1071b581979ee2095b2894539de4e05d
SophonAlpha <SophonAlpha@users.noreply.github.com>
2021-07-20 01:27:52 +0400

    feat: postData function added

--- website/app.js
+++ website/app.js
@@ -1,23 +1,61 @@
 const openWeatherBaseUrl = 'https://api.openweathermap.org/data/2.5/weather';
 const countryCode = 'us';
-const apiKey = '710ce5115e1bf7f0431571fdcd110447_____';
+const apiKey = '710ce5115e1bf7f0431571fdcd110447';
 
 function formSubmit() {
-    console.log('Form submitted.');
-    const ret = getWeather(document.getElementById('zipcode').value);
-    console.log('Data returned:')
-    console.log(ret);
+    document.getElementById('error-message').classList.add('error-msg--hidden');
+    getWeather(
+        document.getElementById('zipcode').value,
+        document.getElementById('userinput').value,
+    );
 }
 
-async function getWeather(zipCode) {
+async function getWeather(zipCode, userInput) {
     const url = openWeatherBaseUrl + '?zip=' + zipCode + ',' + countryCode + '&appid=' + apiKey;
-    const response = await fetch(url);
     try {
-        const data = await response.json();
-        console.log(data);
-        return data;
+        const response = await fetch(url);
+        try {
+            const data = await response.json();
+            if (data['cod'] === 200) {
+                console.log('Response received:')
+                postData(zipCode, data, userInput);
+                console.log(data);
+            } else {
+                console.log('An error occurred when retrieving the response:')
+                showErrorMessage(data['message']);
+            }
+        } catch (error) {
+            console.log('An error occurred when retrieving the response:')
+            console.log(error);
+        }
     } catch (error) {
-        console.log('An error occurred:')
+        console.log('An error occurred during the fetch command:')
         console.log(error);
     }
+}
+
+function showErrorMessage(errorMessage) {
+    const elem = document.getElementById('error-message');
+    elem.innerText = 'An error occurred: ' + errorMessage;
+    elem.classList.remove('error-msg--hidden');
+}
+
+function postData(zipCode, data, userInput) {
+    const time = new Date();
+    const timeStr =
+        time.getFullYear() + '/' +
+        (time.getMonth() + 1) + '/' +
+        time.getDate() + ' ' +
+        time.getHours() + ':' +
+        time.getMinutes() + ':' +
+        time.getSeconds()
+    const postData = {
+        date: timeStr,
+        zipcode: zipCode,
+        city: data.name,
+        weather: data.weather[0].main,
+        temp: Math.round(data.main.temp / 10) + '&deg;C',
+        notes: userInput,
+    };
+    console.log(postData);
 }
